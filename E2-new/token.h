52fba22e7b63dd7a2c60244b4b4c7d274cb89866
Raquel Paradella <73500492+raquelpfm@users.noreply.github.com>
2024-08-12 23:02:05 -0300

    Update token.h

--- E2-new/token.h
+++ E2-new/token.h
@@ -1,1 +1,16 @@
 /* token.h */
+
+#ifndef TOKEN_H
+#define TOKEN_H
+
+typedef enum {
+    EOL = 0,        // newline
+    NUMBER,         // 1
+    PLUS,           // 2
+    MINUS,          // 3
+    TIMES,          // 4
+    DIV,            // 5
+    ERROR           // 6
+} token_t;
+
+#endif
